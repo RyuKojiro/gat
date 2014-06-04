e4668ef2b804c1f7a6d7c9a58c46b96262226577
Daniel Loffgren <daniel@loffgren.org>
2014-06-04 06:30:02 +0000

    Extract just the specified device
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/gat/trunk@10 ccb0ee8c-1eea-e311-9d83-0002a5492622

--- acpi.c
+++ acpi.c
@@ -10,6 +10,7 @@
 
 int main(int argc, const char *argv[]) {
 	int fd, error;
+	const char *mydevname = "acpibat0";
 	prop_dictionary_t battery_dict = prop_dictionary_create();
 
         if ((fd = open(_PATH_SYSMON, O_RDONLY)) == -1)
@@ -18,9 +19,17 @@ int main(int argc, const char *argv[]) {
 	error = prop_dictionary_recv_ioctl(fd, ENVSYS_GETDICTIONARY, &battery_dict);
 	close(fd);
 
-	char *buf = prop_dictionary_externalize(battery_dict);
+       	prop_object_t obj = prop_dictionary_get(battery_dict, mydevname);
+       	if (prop_object_type(obj) != PROP_TYPE_ARRAY) {
+       		warnx("unknown device `%s'", mydevname);
+		prop_object_release(battery_dict);
+		return EXIT_FAILURE;
+        }
+
+	char *buf = prop_array_externalize(obj);
 	printf("%s\n", buf);
-	free(buf); 
+	free(buf);
+
 	prop_object_release(battery_dict);
 	return EXIT_SUCCESS;
 }
