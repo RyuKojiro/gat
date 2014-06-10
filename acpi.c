534b7acb7a5957ca2ed83ee507e183dfcc66283d
Daniel Loffgren <daniel@loffgren.org>
2014-06-10 00:26:28 +0000

    Added some properly list definitions, and fixed indentation
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/gat/trunk@11 ccb0ee8c-1eea-e311-9d83-0002a5492622

--- acpi.c
+++ acpi.c
@@ -8,23 +8,32 @@
 #include <unistd.h>
 #include <paths.h>
 
+#define PROP_KEY_MAXIMUM				"max-value"
+#define PROP_KEY_CRITICAL				"critical-capacity"
+#define PROP_KEY_WARNING				"warning-capacity"
+#define PROP_KEY_CURRENT				"cur-value"
+#define PROP_KEY_DESCRIPTION			"description"
+#define PROP_VALUE_CHARGE_DESCRIPTION	"charge"
+
 int main(int argc, const char *argv[]) {
 	int fd, error;
 	const char *mydevname = "acpibat0";
 	prop_dictionary_t battery_dict = prop_dictionary_create();
 
-        if ((fd = open(_PATH_SYSMON, O_RDONLY)) == -1)
+	if ((fd = open(_PATH_SYSMON, O_RDONLY)) == -1)
 		err(EXIT_FAILURE, "open");
 	
 	error = prop_dictionary_recv_ioctl(fd, ENVSYS_GETDICTIONARY, &battery_dict);
 	close(fd);
 
-       	prop_object_t obj = prop_dictionary_get(battery_dict, mydevname);
-       	if (prop_object_type(obj) != PROP_TYPE_ARRAY) {
-       		warnx("unknown device `%s'", mydevname);
+	prop_object_t obj = prop_dictionary_get(battery_dict, mydevname);
+   	if (prop_object_type(obj) != PROP_TYPE_ARRAY) {
+   		warnx("unknown device `%s'", mydevname);
 		prop_object_release(battery_dict);
 		return EXIT_FAILURE;
-        }
+	}
+
+	
 
 	char *buf = prop_array_externalize(obj);
 	printf("%s\n", buf);
